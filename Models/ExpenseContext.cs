3a7e807754d64d1fc6e649772fc5208d360abd0c
Bianca Tuns <bia.tuns24@gmail.com>
2022-11-15 19:58:29 +0200

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Tuns_Bianca_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options)
+            : base(options) 
+        { 
+        }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+    }
+}
